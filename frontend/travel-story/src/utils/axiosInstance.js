75fe1be00707221479901d4c87d2a8f719165741
Meet-khandelwal <meetkhandelwal212@gmail.com>
2024-11-21 02:03:43 +0530

    Update axiosInstance.js
    updated the base_url with live backend server

--- frontend/travel-story/src/utils/axiosInstance.js
+++ frontend/travel-story/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios"
 //import { BASE_URL } from "./constants"
 
 const axiosInstance = axios.create({
-    baseURL:`${import.meta.env.VITE_BASE_URL}`,
+    baseURL:'https://travel-story-backend.onrender.com',
     timeout:10000,
     headers:{
         "Content-Type":"application/json"
